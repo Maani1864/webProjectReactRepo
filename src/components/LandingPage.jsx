83841a60e43320d033e485d07d25816f51e93da5
Muhammad Usman <sheikhusman1864@gmail.com>
2021-01-01 23:26:19 +0500

    update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import pic1 from "./laptop";
+import pic1 from "./laptop.jpg";
 
 console.log(pic1);
 
@@ -7,6 +7,7 @@ const LandingPage = () => {
 	return (
 		<div>
 			<h1>Landing Page</h1>
+			<img src={laptop} alt="laptop" />
 		</div>
 	);
 };
