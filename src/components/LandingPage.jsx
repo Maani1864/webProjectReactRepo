bafa6741327be20d394efb2a28c1abfbbcf324c2
Muhammad Usman <sheikhusman1864@gmail.com>
2021-01-01 23:17:01 +0500

    pics

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import pic1 from "./laptop";
+
+console.log(pic1);
+
 const LandingPage = () => {
-  return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Landing Page</h1>
+		</div>
+	);
 };
 
 export default LandingPage;
