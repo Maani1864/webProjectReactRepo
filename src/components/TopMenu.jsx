6f26b28f748fbae56cd2d0c70ed9981c68dead5c
Muhammad Usman <sheikhusman1864@gmail.com>
2021-01-09 23:09:17 +0500

    Update TopMenu.jsx

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -28,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
 		"&:hover": {
 			backgroundColor: fade(theme.palette.common.white, 0.25),
 		},
-		marginLeft: "11%",
+		paddingLeft: "8%",
 		width: "100%",
 		[theme.breakpoints.up("sm")]: {
 			marginLeft: theme.spacing(1),
