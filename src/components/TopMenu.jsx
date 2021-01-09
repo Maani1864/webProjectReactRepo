df45ffdebaede4a8f8e9f6f421dfe754c79f8717
Muhammad Usman <sheikhusman1864@gmail.com>
2021-01-09 23:24:46 +0500

    Update TopMenu.jsx

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
 		},
 	},
 	searchIcon: {
-		padding: theme.spacing(0, 2),
+		padding: theme.spacing(0, 8),
 		height: "100%",
 		position: "absolute",
 		pointerEvents: "none",
