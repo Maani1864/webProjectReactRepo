4e274a8f5b8f351a4cd9bb5c4b0f692cb2cdc159
Muhammad Usman <sheikhusman1864@gmail.com>
2021-01-15 01:23:21 +0500

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,12 +1,32 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
+
+const useStyles = makeStyles(() => ({
+	heading: {
+		textAlign: "center",
+		paddingTop: "2%",
+	},
+	pFonts: {
+		paddingLeft: "28%",
+		fontSize: "14px",
+	},
+}));
+
 const ContactUs = () => {
+	const classes = useStyles();
 	return (
 		<div>
-			<h1>Contact Us</h1>
-			<p>Contact No: +92xxx xxxxxxx</p>
+			<h1 className={classes.heading}>Contact Us</h1>
+			<br />
+			<br />
+			<br />
+			<p className={classes.pFonts}>Contact No: +92xxx xxxxxxx</p>
+			<br />
 			<hr style={{ width: "15%", marginLeft: 0 }} />
-			<p>Gmail: xxxxxxx@gmail.com</p>
-			<p>Yahoo: xxxxxxx@yahoo.com</p>
+			<br />
+			<p className={classes.pFonts}>Gmail: xxxxxxx@gmail.com</p>
+			<br />
+			<p className={classes.pFonts}>Yahoo: xxxxxxx@yahoo.com</p>
 		</div>
 	);
 };
